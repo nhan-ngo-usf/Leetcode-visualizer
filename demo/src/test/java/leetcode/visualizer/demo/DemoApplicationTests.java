e0ddce645a8b9fc9ed7b7206bfaf188cff6ff862
Nhan <nhan.t.ngo@outlook.com>
2024-02-09 15:33:52 -0500

    =springboot loadup

---
+++ demo/src/test/java/leetcode/visualizer/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package leetcode.visualizer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
